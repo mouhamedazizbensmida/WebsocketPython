b0adaa07681639996dba69131f0d75f5c052f50e
Med Aziz Ben Smida <73040491+mouhamedazizbensmida@users.noreply.github.com>
2024-08-14 15:08:28 +0200

    app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ async def counter(websocket, path):
         websockets.broadcast(USERS.keys(), students_event())
 
 async def main():
-    async with websockets.serve(counter, "localhost", 6789):
+    async with websockets.serve(counter, "websocketpython-onywqmaszluv84bopmsxho.streamlit.app", 6789):
         await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
